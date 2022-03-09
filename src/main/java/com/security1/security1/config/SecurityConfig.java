cd035ecf61959ee5ff031a3d4a549cc29b2c883d
leeyoungchun <dldydcns123@gmail.com>
2022-03-09 13:30:35 +0900

    시큐리티 회원가입, 로그인 구현

--- src/main/java/com/security1/security1/config/SecurityConfig.java
+++ src/main/java/com/security1/security1/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package com.security1.security1.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    //해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -19,6 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/login");
+                .loginPage("/loginForm")
+//                .usernameParameter("username2")
+                .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
+                .defaultSuccessUrl("/");
     }
 }
--- src/main/java/com/security1/security1/controller/IndexController.java
+++ src/main/java/com/security1/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.security1.security1.controller;
 
+import com.security1.security1.model.User;
+import com.security1.security1.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.persistence.EntityManager;
+
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @GetMapping({"","/"})
     public String index(){
@@ -23,16 +35,21 @@ public class IndexController {
     public @ResponseBody String manager(){
         return "manager";
     }
-    @GetMapping("/login")
-    public String login(){
+    @GetMapping("/loginForm")
+    public String loginForm(){
         return "loginForm";
     }
-    @GetMapping("/join")
-    public @ResponseBody String join(){
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
     }
-    @GetMapping("/joinProc")
-    public @ResponseBody String joinProc(){
-        return "회원가입 완료됨";
+    @PostMapping("/join")
+    public String join(User user){
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user); //비밀번호 1234 => 시큐리티로 로그인 할 수 없음, 비번이 암호화가 안됐기때문에 암호화를 해야함
+        return "redirect:/loginForm";
     }
 }
